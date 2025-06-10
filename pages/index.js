472a469c8cefd418fba02302d011076bb572954c
Saad Ali <saadabouelsooud@users.noreply.github.com>
2025-06-10 10:10:13 +0300

    Remove hero section horizontal padding

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
       </Head>
       <Navbar />
       <main className="scroll-smooth">
-        <section className="min-h-screen flex items-center px-4 bg-gradient-to-br from-[#6666f2] to-[#885df5]">
+        <section className="min-h-screen flex items-center px-0 bg-gradient-to-br from-[#6666f2] to-[#885df5]">
           <div className="flex flex-col md:flex-row justify-start items-start max-w-7xl mx-auto gap-12">
             <div className="text-white space-y-4 text-center md:text-left flex-1">
               <span className="inline-flex items-center text-xs font-semibold text-white bg-gradient-to-r from-[#6666f2]/80 to-[#885df5]/80 px-4 py-1 rounded-full w-max">
